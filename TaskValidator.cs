106fdd9c1dd367bcbe5415345f795f551ffefa83
Malav Patel <malavpatel501@gmail.com>
2023-05-28 22:12:08 +1000

    added separate task validator class and changed FileParser accordingly. added case 2,3,4 in the menu

---
+++ TaskValidator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A3_ADT
+{
+    public static class TaskValidator
+    {
+        public static bool ValidateLine (string line)
+        {
+            //split the line by comma
+            string[] parts = line.Split(',');
+
+            //check if the line has at least minimum of two parts (task ID and time)
+            if (parts.Length < 2)
+            {
+                Console.WriteLine("Invalid line format. The should contain at least two items (task ID and its processing time)");
+                return false;
+            }
+
+            //validate task ID to be added
+            string taskID = parts[0].Trim();
+            if (string.IsNullOrEmpty(taskID))
+            {
+                Console.WriteLine($"Invalid line format. Task ID is empty. '{taskID}'");
+                return false;
+            }
+
+            //validate time of the task to be added
+            if (!int.TryParse(parts[1].Trim(), out int timeNeeded) || timeNeeded <= 0)
+            {
+                Console.WriteLine($"Invalid time format. '{timeNeeded}'");
+                return false;
+            }
+
+            //validate the dependencies if of the task to be added if any
+            for (int i = 2; i < parts.Length; i++)
+            {
+                string dependency = parts[i].Trim();
+
+                if (string.IsNullOrEmpty(dependency))
+                {
+                    Console.WriteLine($"Invalid task dependency. '{dependency}'");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- menu.cs
+++ menu.cs
@@ -4,26 +4,37 @@ namespace A3_ADT
 {
     public class Menu
     {
+        private TaskList taskList;
+
+        public Menu()
+        {
+            taskList = new TaskList();
+        }
+
         public void DisplayMenu()
         {
             bool exit = false;
 
             while (!exit)
             {
+                //refreshes the menu screen
                 Console.Clear();
 
+                //print menu
                 Console.WriteLine("------ PROJECT MANAGEMENTMENT SYSTEM ------");
                 Console.WriteLine();
                 Console.WriteLine("1. Load tasks from the file");
                 Console.WriteLine("2. Add a new task");
                 Console.WriteLine("3. Remove a task");
                 Console.WriteLine("4. Update completion time");
-                Console.WriteLine("5. Save tasks to the file");
-                Console.WriteLine("6. Find task sequence");
-                Console.WriteLine("7. Calculate earliest completion times");
-                Console.WriteLine("8. Exit");
+                Console.WriteLine("5. Display the current tasks");
+                Console.WriteLine("6. Save tasks to the file");
+                Console.WriteLine("7. Find task sequence");
+                Console.WriteLine("8. Calculate earliest completion times");
+                Console.WriteLine("9. Exit");
                 Console.WriteLine();
 
+                //prompt user to select an option
                 Console.WriteLine("Select an option: ");
                 string userInput = Console.ReadLine();
 
@@ -31,7 +42,7 @@ namespace A3_ADT
                 switch (userInput)
                 {
                     case "1":
-                        Console.WriteLine("Loading tasks from the file");
+                        Console.WriteLine("Loading tasks from the file...");
 
                         //get the file name from user
                         Console.WriteLine("Enter the file name: ");
@@ -41,6 +52,7 @@ namespace A3_ADT
                         //instantiate FileParser class to load the file
                         FileParser fileParser = new FileParser(fileName);
 
+                        //check for the task loading
                         if (fileParser.ParsedData.Count > 0)
                         {
                             Console.WriteLine("Tasks loaded successfully.");
@@ -54,20 +66,59 @@ namespace A3_ADT
                         {
                             Console.WriteLine("Error: The file is empty or invalid.");
                         }
-
                         break;
 
                     case "2":
-                        Console.WriteLine("Option 2 selected");
+                        Console.WriteLine("Enter new task (ID, Time, Dependencies if any): ");
+                        string task = Console.ReadLine();
+
+                        //validate the format
+                        if (TaskValidator.ValidateLine(task))
+                        {
+                            taskList.AddTask(task);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid task format. Task '{task}' not added");
+                        }
                         break;
 
                     case "3":
-                        Console.WriteLine("Option 3 selected");
-                        break;
+                        Console.WriteLine("Enter new taskID to be removed: ");
+                        string taskID = Console.ReadLine();
+
+                        if (string.IsNullOrEmpty(taskID))
+                        {
+                            Console.WriteLine("Invalid task ID.");
+                            break;
+                        }
+                        else
+                        {
+                            taskList.RemoveTask(taskID);
+                            break;
+                        }
 
                     case "4":
-                        Console.WriteLine("Option 4 selected");
-                        break;
+                        Console.WriteLine("Enter taskID: ");
+                        string tID = Console.ReadLine();
+
+                        if (string.IsNullOrEmpty(tID))
+                        {
+                            Console.WriteLine("Invalid task ID."); break;
+                        }
+
+                        Console.WriteLine("Enter the new time: ");
+                        string input = Console.ReadLine();
+                        bool isNum = int.TryParse(input, out int newTime);
+
+                        if (isNum)
+                        {
+                            taskList.UpdateTimeCompletion(tID, newTime); break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid time format."); break;
+                        }
 
                     case "5":
                         Console.WriteLine("Option 5 selected");
@@ -82,6 +133,10 @@ namespace A3_ADT
                         break;
 
                     case "8":
+                        Console.WriteLine("Option 8 selected");
+                        break;
+
+                    case "9":
                         Console.WriteLine("Exiting the program...");
                         exit = true;
                         break;
