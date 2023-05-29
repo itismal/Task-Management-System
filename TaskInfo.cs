55703f9b5a095015548445fc17ac857066278c62
Malav Patel <malavpatel501@gmail.com>
2023-05-29 20:34:23 +1000

    added taskinfo class for task characteristics

---
+++ TaskInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A3_ADT
+{
+    public class TaskInfo
+    {
+        public string TaskID { get; }
+        public int TimeNeeded { get; }
+        public List<string> Dependencies { get; }
+
+        public TaskInfo (string taskID, int timeNeeded, List<string> dependencies)
+        {
+            TaskID = taskID;
+            TimeNeeded = timeNeeded;
+            Dependencies = dependencies;
+        }
+    }
+}
